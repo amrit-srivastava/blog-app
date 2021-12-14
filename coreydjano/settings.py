5e6fecac542b9146146d25eedc8a088969463afe
amrit-srivastava <amrit.srivastava@gmail.com>
2021-12-14 16:53:07 +0530

    modify settings

--- coreydjano/settings.py
+++ coreydjano/settings.py
@@ -123,13 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "blog-home"
 
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
